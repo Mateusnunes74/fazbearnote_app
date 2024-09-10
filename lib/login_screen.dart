f4c85c159f61af7a1b52e9d1cb6804ed245f850b
Mateus Nunes <mateusnume1213@gmail.com>
2024-09-10 20:12:30 -0300

    first commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Controllers for the TextFields
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FazBearNote'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Central Image
+            Center(
+              child: Image.asset(
+                'assets/image.png',
+                width: 214,
+                height: 302,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 14),
+            TextField(
+              controller: _controller1,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Digite seu email',
+              ),
+            ),
+            const SizedBox(height: 14),
+            // Second TextField
+            TextField(
+              controller: _controller2,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Digite sua senha',
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                 backgroundColor: const Color.fromARGB(255, 74, 6, 219),
+              ),
+              onPressed: () {
+              },
+              child: const Text(
+                'Iniciar',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255)
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
